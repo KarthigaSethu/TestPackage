a52be1478d2028be4ac45c17ae3f097d05172779
Karthiga Sethu <karthigaksk@gmail.com>
2024-01-19 15:29:57 -0800

    Test

---
+++ R/printsampledata.R
@@ -0,0 +1,4 @@
+print_data<-function(){
+  my_data <- sendsampledata::send_data()
+  print(my_data)
+}
---
+++ R/sendsampledata.R
@@ -0,0 +1,8 @@
+send_data<-function(){
+  my_data <- data.frame(
+    Name = c("Alice", "Bob", "Charlie"),
+    Age = c(25, 30, 22),
+    City = c("New York", "San Francisco", "Los Angeles")
+  )
+  my_data
+}
